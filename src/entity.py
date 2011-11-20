d1124774b7edb5afdfbda3329d395130082c6690
Stanislav Vishnevskiy <svishnevskiy@watercooler-inc.com>
2011-11-19 20:01:57 -0800

    put limiter in own file

--- src/entity.py
+++ src/entity.py
@@ -1,26 +1,6 @@
 from datetime import datetime, timedelta
 import random
-
-class Limiter(object):
-    def __init__(self, ms=100, randomize=False):
-        self.randomize = randomize
-        self.set_delta(ms)
-
-    def set_delta(self, ms):
-        if self.randomize:
-            ms = random.randint(ms * 0.5, ms * 2)
-
-        self.delta = timedelta(milliseconds=ms)
-        self.last_time = None
-
-    def is_ready(self):
-        now = datetime.now()
-
-        if self.last_time is None or self.last_time <= now - self.delta:
-            self.last_time = now
-            return True
-
-        return False
+from limiter import Limiter
 
 BASE_STATS = {
     'hp': 0,
---
+++ src/limiter.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from random import randint
+
+class Limiter(object):
+    def __init__(self, ms=100, randomize=False):
+        self.randomize = randomize
+        self.set_delta(ms)
+
+    def set_delta(self, ms):
+        if self.randomize:
+            ms = randint(ms * 0.5, ms * 2)
+
+        self.delta = timedelta(milliseconds=ms)
+        self.last_time = None
+
+    def is_ready(self):
+        now = datetime.now()
+
+        if self.last_time is None or self.last_time <= now - self.delta:
+            self.last_time = now
+            return True
+
+        return False
