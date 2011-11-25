81271140d615cec2a01a77ff2c73b1d0c2d59876
Stanislav Vishnevskiy <vishnevskiy@gmail.com>
2011-11-25 02:25:24 -0800

    delete unused files

--- src/venom/instance.py
+++ src/venom/instance.py
@@ -1,4 +1,3 @@
-import random
 import tornado.ioloop
 import datetime
 import simplejson
@@ -64,10 +63,12 @@ class Instance(object):
             x, y = (168, 107)
             faction = 'pink'
 
-        sprite = Sprite('character/%s.png' % random.choice(['cray', 'ryu', 'nina']), walk=6, stand=1)
+        player = self.spawn(name, x, y, uid=conn.uid, faction=faction, kind=PlayerEntity, sprite=Sprite([
+            'character/nina.png',
+            'character/ryu.png',
+            'character/cray.png',
+        ], walk=6, stand=1), **kwargs)
 
-        player = self.spawn(name, x, y, uid=conn.uid, faction=faction, kind=PlayerEntity,
-            sprite=sprite, **kwargs)
         player.connections.add(conn)
 
         self.players.add(player)
